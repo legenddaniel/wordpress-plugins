9ed12f6c257bdf092ea4c62d96d1651e926cb3da
legenddaniel <53063815+legenddaniel@users.noreply.github.com>
2020-11-16 15:18:38 -0500

    Update admin.js

--- sz-custom-booking/js/admin.js
+++ sz-custom-booking/js/admin.js
@@ -1,4 +1,11 @@
 jQuery(document).ready(function ($) {
+
+    // Only in product editing page
+    if (!/post\.php\?post=\d+&action=edit/.test(window.location.pathname)) {
+        return;
+    }
+
+    // Toggle the discount field in product editing page
     $('.sz-admin-checkbox-enable').on('change', function () {
         const $textfield = $(this).closest('.form-field').next('.form-field');
         if (this.checked) {
@@ -8,8 +15,8 @@ jQuery(document).ready(function ($) {
         }
     });
 
+    // Change the display and styling of discount field in booking details page
     const $discountInfoLabel = $('#booking_data .booking_data_column_container .booking_data_column:first-of-type .form-field:nth-of-type(5) label');
 
     $discountInfoLabel.text('Discount Info: ').attr('class', 'sz-admin-booking-subtitle');
-
 })
