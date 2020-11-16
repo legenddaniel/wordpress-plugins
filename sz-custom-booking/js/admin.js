dd8f4a46df5828c0e330d0b92deac05eeb39e009
legenddaniel <53063815+legenddaniel@users.noreply.github.com>
2020-11-16 15:02:50 -0500

    update

--- sz-custom-booking/js/admin.js
+++ sz-custom-booking/js/admin.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function ($) {
-    $('.sz-admin-byoe-enable').on('change', function () {
+    $('.sz-admin-checkbox-enable').on('change', function () {
         const $textfield = $(this).closest('.form-field').next('.form-field');
         if (this.checked) {
             $textfield.removeClass('d-none');
@@ -7,4 +7,9 @@ jQuery(document).ready(function ($) {
             $textfield.addClass('d-none');
         }
     });
+
+    const $discountInfoLabel = $('#booking_data .booking_data_column_container .booking_data_column:first-of-type .form-field:nth-of-type(5) label');
+
+    $discountInfoLabel.text('Discount Info: ').attr('class', 'sz-admin-booking-subtitle');
+
 })
