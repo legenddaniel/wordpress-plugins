f4eb79779bbde768f43471deb5f87d803ef08667
legenddaniel <siyuanincanada@gmail.com>
2020-11-28 18:59:55 -0500

    update

--- sz-custom-booking/js/resource.js
+++ sz-custom-booking/js/resource.js
@@ -1,17 +1,29 @@
 jQuery(document).ready(function ($) {
+
+    // var types = [2997, 2998, 2999]; // For test
+    // var types = [70541, 70542, 70543]; // For real
+
+    var $persons = $('#wc-bookings-booking-form > p');
+
+    // Find types from the persons class names
+    var types = [];
+    $persons.each(function() {
+        var type = $(this).attr('class').match(/\d+/)[0];
+        types.push(type);
+    })
+    
     var $resourcesHtml = `
         <label for="sz-resources">Type:</label>
         <select id="sz-resources" name="sz-resources" class="form-field form-field-wide" data-persons="1">
-            <option value="70541" selected>Archery</option>
-            <option value="70542">Airsoft</option>
-            <option value="70543">Combo</option>
+            <option value="${types[0]}" selected>Archery</option>
+            <option value="${types[1]}">Airsoft</option>
+            <option value="${types[2]}">Combo</option>
         </select>
     `;
 
     // Add custom resource select
     $('fieldset.wc-bookings-date-picker').before($resourcesHtml);
 
-    var $persons = $('#wc-bookings-booking-form > p');
     var $resources = $('#sz-resources');
 
     // Set label text of persons to 'Persons'
--- sz-custom-booking/sz-custom-booking.php
+++ sz-custom-booking/sz-custom-booking.php
@@ -29,22 +29,42 @@ require_once $plugin_dir . 'includes/vip.php';
 // ---------------Config Area Starts
 
 // For real
-define('SINGULAR_ID', 68051);
-define('PROMO_ID', 68067);
+// define('SINGULAR_ID', 68051);
+// define('PROMO_ID', 68067);
 
-define('ARCHERY_ID', 70541);
-define('AIRSOFT_ID', 70542);
-define('COMBO_ID', 70543);
+// define('ARCHERY_ID', 70541);
+// define('AIRSOFT_ID', 70542);
+// define('COMBO_ID', 70543);
 
-define('VIP_ANNUAL_ID', 68456);
-define('VIP_SEMIANNUAL_ID', 68463);
+// define('ARCHERY_ACT_ID', 70057);
+// define('AIRSOFT_ACT_ID', 70055);
+// define('COMBO_ACT_ID', 70056);
+
+// define('ARCHERY_PROMO_ID', 68068);
+// define('AIRSOFT_PROMO_ID', 68069);
+// define('COMBO_PROMO_ID', 68070);
+
+// define('VIP_ANNUAL_ID', 68456);
+// define('VIP_SEMIANNUAL_ID', 68463);
 
 // For test
-// define('SINGULAR_ID', 304);
-// define('PROMO_ID', 358);
-// define('ARCHERY_ID', 291);
-// define('AIRSOFT_ID', 292);
-// define('COMBO_ID', 293);
+define('SINGULAR_ID', 2996);
+define('PROMO_ID', 3004);
+
+define('ARCHERY_ID', 2997);
+define('AIRSOFT_ID', 2998);
+define('COMBO_ID', 2999);
+
+define('ARCHERY_ACT_ID', 3015);
+define('AIRSOFT_ACT_ID', 3016);
+define('COMBO_ACT_ID', 3017);
+
+define('ARCHERY_PROMO_ID', 3005);
+define('AIRSOFT_PROMO_ID', 3006);
+define('COMBO_PROMO_ID', 3007);
+
+define('VIP_ANNUAL_ID', 3001);
+define('VIP_SEMIANNUAL_ID', 3002);
 
 // For local
 // define('SINGULAR_ID', 7);
