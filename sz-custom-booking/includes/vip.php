f60035dcb1efc9e2b410500b4157b156e7a715ac
legenddaniel <53063815+legenddaniel@users.noreply.github.com>
2020-12-11 19:38:30 -0500

    update

--- sz-custom-booking/includes/vip.php
+++ sz-custom-booking/includes/vip.php
@@ -3,7 +3,7 @@
 // Some VIP functionalities
 
 /**
- * Renew VIP scheduled task
+ * Renew VIP weekly scheduled task (reset discount)
  * @param int $user
  * @param int $plan
  * @return null
@@ -30,11 +30,12 @@ add_action('sz_cron_vip', 'sz_renew_vip_in_db', 10, 2);
  *@type int|string $user_id user ID for the membership
  *@type int|string $user_membership_id post ID for the new user membership
  *@type bool $is_update true if the membership is being updated, false if new
- * Daniel: $is_update is always tested true
  *}
  */
-function sz_manage_vip_field_in_db($membership_plan, $arguments)
+function sz_manage_vip_field_in_db_saved($membership_plan, $arguments)
 {
+    // These codes are valid for all cases, except for renewing membership in the front end. Use sz_manage_vip_field_in_db_accessed defined next for this special case.
+
     $plans = [VIP_888_ANNUAL_ID, VIP_ANNUAL_ID, VIP_SEMIANNUAL_ID];
     $plan = $membership_plan->id;
     if (!in_array($plan, $plans)) {
@@ -49,7 +50,7 @@ function sz_manage_vip_field_in_db($membership_plan, $arguments)
 
         $last_deactivation_timestamp = get_post_meta($user_plan, 'activation_end', true);
 
-        // Set up Guest Pass field for VIP 888 in db. So far only set up this field when this user membership initialized
+        // Set up Guest Pass field for VIP 888 in db at first purchase and renewing (no manual deactivation detected).
         if ($plan === VIP_888_ANNUAL_ID && !$last_deactivation_timestamp) {
             update_user_meta($user, 'Guest', GUEST_QTY);
         }
@@ -70,17 +71,63 @@ function sz_manage_vip_field_in_db($membership_plan, $arguments)
 
         // Remove the old CRON
         // remove_action('sz_cron_vip', 'sz_renew_vip_in_db');
+
         wp_unschedule_event(wp_next_scheduled('sz_cron_vip', $args), 'sz_cron_vip', $args);
         date_default_timezone_set('UTC');
 
-        // Add/update activation_end meta key for the date validation above
-        update_post_meta($user_plan, 'activation_end', time());
+        if ((new WC_Memberships_User_Membership($user_plan))->get_status() === 'expired') {
 
-        // delete_user_meta($user, 'VIP');
+            // Clear reset restriction when expired
+            delete_post_meta($user_plan, 'activation_end');
+        } else {
+
+            // Add/update activation_end meta key for the date validation above
+            update_post_meta($user_plan, 'activation_end', time());
+
+            // delete_user_meta($user, 'VIP');
+        }
 
     }
 }
-add_action('wc_memberships_user_membership_saved', 'sz_manage_vip_field_in_db', 10, 2);
+add_action('wc_memberships_user_membership_saved', 'sz_manage_vip_field_in_db_saved', 10, 2);
+
+/**
+ * @param WC_Memberships_Membership_Plan $membership_plan
+ * @param array $args {
+ *@type int|string $user_id user ID for order
+ *@type int|string $product_id product ID that grants access
+ *@type int|string $order_id order ID
+ *@type int|string $user_membership_id post ID for the new user membership
+ *}
+ */
+function sz_manage_vip_field_in_db_accessed($membership_plan, $arguments)
+{
+    // For the case of membership purchase in the front end (new or renewed)
+
+    $plans = [VIP_888_ANNUAL_ID, VIP_ANNUAL_ID, VIP_SEMIANNUAL_ID];
+    $plan = $membership_plan->id;
+    if (!in_array($plan, $plans)) {
+        return;
+    }
+
+    $user = $arguments['user_id'];
+    $user_plan = $arguments['user_membership_id'];
+    $args = [$user, $plan];
+
+    // Set up Guest Pass field for VIP 888 in db at first purchase and renewing.
+    if ($plan === VIP_888_ANNUAL_ID) {
+        update_user_meta($user, 'Guest', GUEST_QTY);
+    }
+
+    if (!wp_next_scheduled('sz_cron_vip', $args)) {
+        date_default_timezone_set('America/Toronto');
+        wp_schedule_event(strtotime('next Monday'), 'weekly', 'sz_cron_vip', $args);
+    }
+
+    sz_renew_vip_in_db($user, $plan);
+
+}
+add_action('wc_memberships_grant_membership_access_from_purchase', 'sz_manage_vip_field_in_db_accessed', 10, 2);
 
 /**
  * Do not grant membership access to purchasers if they already hold an active membership
